4cab18ba88af9d7c3bdff4be5d5397692244d829
ipopop <popop@ntymail.com>
2020-11-11 12:32:28 +0100

    add list - ok

--- src/app/services/techno.service.ts
+++ src/app/services/techno.service.ts
@@ -1,16 +1,25 @@
 import { Injectable } from '@angular/core';
+import { Technology } from '../models/technology';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TechnoService {
-  private technos = [];
+  private technos: Technology[] = [];
 
   constructor() { }
 
-  createTechno(techno) {
+  createTechno(techno: Technology) {
     const newTechno = { id: Date.now(), ...techno };
     this.technos = [newTechno, ...this.technos];
     console.log(this.technos);
+  }
+
+  getTechnos(): Technology[] {
+    return this.technos;
+  }
+
+  deleteTechno(techno: Technology) {
+    this.technos = this.technos.filter(t => t.id !== techno.id);
   }
 }
---
+++ src/app/techno-list/techno-list.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { Technology } from '../models/technology';
+import { TechnoService } from '../services/techno.service';
+
+@Component({
+  selector: 'app-techno-list',
+  templateUrl: './techno-list.component.html',
+  styleUrls: ['./techno-list.component.css']
+})
+export class TechnoListComponent implements OnInit {
+  allTechnos = [];
+
+  constructor(private ts: TechnoService) { }
+
+  ngOnInit(): void {
+    this.getTechnos();
+  }
+
+  getTechnos() {
+    this.allTechnos = this.ts.getTechnos();
+  }
+
+  deleteTechno(techno: Technology) {
+    console.log('deleteTechno', techno);    
+    this.ts.deleteTechno(techno);
+    this.allTechnos = this.ts.getTechnos();
+  }
+
+}
